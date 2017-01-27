6db436cbc4d7a6738d5b00e5ea1f8176ef6c0621
kylavana <kyla.extra@gmail.com>
2017-01-27 16:01:10 -0600

    js files for new version

---
+++ js/functions.js
@@ -0,0 +1,263 @@
+/*///LIGHTBOX/////*/
+  var userscroll;
+
+//open lightbox
+function expand_lb(clicked_el) {
+ 
+    userscroll = document.body.scrollTop;
+
+    window.scrollTo(0, 0);   
+  
+  //add expand class
+ var lb_el = clicked_el.nextElementSibling;
+   if (lb_el.classList)
+  lb_el.classList.add('expand');
+else
+ lb_el.className += ' ' + 'expand';
+  
+  //add container hide
+  var container = document.querySelectorAll(".container");
+[].forEach.call(container, function(el1) {
+   if (container.classList)
+  el1.classList.add('container-hide');
+else
+el1.className += ' ' + 'container-hide';
+});  
+
+  };
+  
+//close lightbox
+  function close_lb() {
+  
+   //remove expand class
+  var lbs = document.querySelectorAll(".lightbox.expand");
+[].forEach.call(lbs, function(el2) {
+  el2.className = el2.className.replace(/\bexpand\b/, "");
+});  
+    
+//remove container hide class
+  var cont_hide = document.querySelectorAll(".container.container-hide");
+[].forEach.call(cont_hide, function(el3) {
+  el3.className = el3.className.replace(/\bcontainer-hide\b/, "");
+});  
+    
+window.scrollTo(0, userscroll);
+
+};
+
+//add small menu on scroll
+window.onscroll = function(){
+win_scroll();
+anim_skill_icons();
+};
+ 
+function win_scroll() {
+  var scroll_top = document.body.scrollTop;
+    var header = document.getElementById("header-side");
+  
+  if (scroll_top > 40){
+if (header.classList)
+  header.classList.add('header-side-show');
+else
+  header.className += ' ' + 'header-side-show';
+  }else{
+      header.className = header.className.replace(/\bheader-side-show\b/, "");
+  } 
+}
+
+/*///ANIMATE ICONS ON SCROLL*/
+function isScrolledIntoView(elem) {
+    var docViewTop = window.scrollY;
+    var docViewBottom = docViewTop + window.innerHeight;
+
+var rect = elem.getBoundingClientRect();
+var elemTop = rect.top + document.body.scrollTop;
+  
+var elemBottom = elemTop + elem.offsetHeight;
+
+return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+function anim_skill_icons() {
+ var elements = document.querySelectorAll('.skill-pic');
+Array.prototype.forEach.call(elements, function(el, i){
+   if (isScrolledIntoView(el) === true) {
+            
+  if (el.classList)
+  el.classList.add('in-view');
+else
+  el.className += ' ' + 'in-view'; 
+        }
+}); 
+  
+   var elements2 = document.querySelectorAll('.footer');
+Array.prototype.forEach.call(elements2, function(el, i){
+   if (isScrolledIntoView(el) === true) {
+            
+  if (el.classList)
+  el.classList.add('footer-grow');
+else
+  el.className += ' ' + 'footer-grow'; 
+        }
+}); 
+};
+
+/*///SMOOTH SCROLL///*/
+initSmoothScrolling();
+
+function initSmoothScrolling() {
+  if (isCssSmoothSCrollSupported()) {
+    document.getElementById('css-support-msg').className = 'supported';
+    return;
+  }
+
+  var duration = 400;
+
+  var pageUrl = location.hash ?
+      stripHash(location.href) :
+      location.href;
+
+  delegatedLinkHijacking();
+  //directLinkHijacking();
+
+  function delegatedLinkHijacking() {
+    document.body.addEventListener('click', onClick, false);
+
+    function onClick(e) {
+      var target = e.target;
+      if (!isInPageLink(target)) {
+        var output = isInPageLinkRecurse(target);
+        if(output != false) {
+          target = output;
+        } else {
+          return;
+        }
+
+      }
+
+
+      e.stopPropagation();
+      e.preventDefault();
+
+      jump(target.hash, {
+        duration: duration,
+        callback: function() {
+          setFocus(target.hash);
+        }
+      });
+    }
+  }
+
+  function directLinkHijacking() {
+    [].slice.call(document.querySelectorAll('a'))
+        .filter(isInPageLink)
+        .forEach(function(a) {
+          a.addEventListener('click', onClick, false);
+        });
+
+    function onClick(e) {
+      e.stopPropagation();
+      e.preventDefault();
+
+      jump(e.target.hash, {
+        duration: duration
+      });
+    }
+
+  }
+
+  function isInPageLink(n) {
+    return n.tagName.toLowerCase() === 'a' &&
+        n.hash.length > 0 &&
+        stripHash(n.href) === pageUrl;
+  }
+
+  var count = 0;
+  var isInPageLinkRecurse = function recurse (target) {
+    var parent = target.parentElement;
+    var linkFound = isInPageLink(parent);
+    if (linkFound) {
+      count = 0;
+      return parent;
+    } else if (count < 4){
+      count++;
+      return recurse(parent);
+    } else {
+      count = 0;
+      return linkFound;
+    }
+  };
+
+  function stripHash(url) {
+    return url.slice(0, url.lastIndexOf('#'));
+  }
+
+  function isCssSmoothSCrollSupported() {
+    return 'scrollBehavior' in document.documentElement.style;
+  }
+
+  // Adapted from:
+  // https://www.nczonline.net/blog/2013/01/15/fixing-skip-to-content-links/
+  function setFocus(hash) {
+    var element = document.getElementById(hash.substring(1));
+
+    if (element) {
+      if (!/^(?:a|select|input|button|textarea)$/i.test(element.tagName)) {
+        element.tabIndex = -1;
+      }
+
+      element.focus();
+    }
+  }
+
+}
+
+function jump(target, options) {
+  var
+      start = window.pageYOffset,
+      opt = {
+        duration: options.duration,
+        offset: options.offset || 0,
+        callback: options.callback,
+        easing: options.easing || easeInOutQuad
+      },
+      distance = typeof target === 'string' ?
+      opt.offset + document.querySelector(target).getBoundingClientRect().top :
+          target,
+      duration = typeof opt.duration === 'function' ?
+          opt.duration(distance) :
+          opt.duration,
+      timeStart, timeElapsed;
+
+  requestAnimationFrame(function(time) {
+    timeStart = time;
+    loop(time);
+  });
+
+  function loop(time) {
+    timeElapsed = time - timeStart;
+
+    window.scrollTo(0, opt.easing(timeElapsed, start, distance, duration));
+
+    if (timeElapsed < duration)
+      requestAnimationFrame(loop);
+    else
+      end();
+  }
+
+  function end() {
+    window.scrollTo(0, start + distance);
+
+    if (typeof opt.callback === 'function')
+      opt.callback();
+  }
+
+  // Robert Penner's easeInOutQuad - http://robertpenner.com/easing/
+  function easeInOutQuad(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b
+  }
+
+}
---
+++ js/portfolio.js
@@ -0,0 +1,114 @@
+
+// Define the `portfolioApp` module
+var app = angular.module("app", []);
+
+//define `yearController`
+app.controller('yearController', function ($scope) {
+$scope.date = new Date();
+});
+
+// Define the `portfolioController`
+app.controller('portfolioController', function ($scope) {
+  $scope.portfolioItems = [
+    {
+      title: 'Campus Map',
+      subtitle: 'University of Texas Medical Branch',
+      image:'port_utmb.jpg',
+      desc:'The UTMB campus infrastructure was severely damaged by flood waters caused by Hurricane Ike. This map illustrates proposed infrastructure upgrades designed to increase energy effeciency and storm resiliance. This illustration is used by Affiliated Engineers, Inc. for several of their marketing pieces regarding the UTMB campus, and infrastructure resiliance.',
+      link:'/port_utmb_big.jpg',
+      link_class:'icon-pic',
+      link_txt:'View full size',
+    },
+    {
+      title: 'Corporate Website',
+      subtitle: 'Affiliated Engineers, Inc.',
+      image:'port_aeieng.png',
+      desc:"Affiliated Engineers, Inc. needed an upgrade of their old, static website which could not be easily maintained. My solution was to write a custom Content Management system in php. The new content mamnagement system takes data from AEI's existing databases and uses that information to dynamically populate the website's pages. I also redesigned the look of the website, and made the pages responsive to better support AEI's mobile users.",
+     link:'http://www.aeieng.com/',//set to no to hide
+      link_class:'icon-link',
+      link_txt:'Visit the website',
+    },
+    {
+      title: 'Happy Birthday, FiL!',
+      subtitle: "Digital Booklet",
+      image:'port_bday.jpg',
+      desc:"A web-based booklet celebrating the 1-year anniversary of Affiliated Engineers, Inc.'s new knowledge sharing platform (nick-named FiL). The booklet features a user driven page-turning effect (only on desktops) and animation to help bring the illustrations to life. The illsutrations were drawn with Adobe Illustrator, and animated by css.",
+     link:'http://www.aeieng.com/images/misc/fil_bday.html',//set to no to hide
+      link_class:'icon-link',
+      link_txt:'View the booklet',
+    },
+    {
+      title: 'Dungeons & Javascript',
+      subtitle: "Text-based RPG",
+      image:'port_dnj.jpg',
+      desc:"Dungeons & Javascript (D&J) is a simple text-based dungeon crawling game with some rogue-like features. The goal is to get through as many rooms as possible.  D&J is written entirely in Javascript, css, and html.  Also... ASCII art!",
+     link:'/dnj/dnj.html',//set to no to hide
+      link_class:'icon-play',
+      link_txt:'Play it!',
+    },
+    {
+      title: 'Absolutely',
+      subtitle: 'Sticker Artwork',
+      image:'port_abso.jpg',
+      desc:'Sticker artwork designed as a hand out for participants of the Greenbuild 2010 conference. "Aboslutely" refers to absolute metrics when determining the sustainability rating of a structure. This contrasts with relative metrics, which is the more often used (though less accurate) rating method.',
+     link:'no',//set to no to hide
+      link_class:'',
+      link_txt:'',
+    },
+    {
+      title: 'Mechanical Floor',
+      subtitle: "Ann & Robert H. Lurie Children's Hospital of Chicago.",
+      image:'port_lurie.jpg',
+      desc:"An illustration done for Affiliated Engineers Inc. of the mechanical level in the Ann & Robert H. Lurie Children's Hospital of Chicago. The hospital is noteworthy for it's vertical orientation (hospitals are almost always laid-out horizontally) and for the complex interaction between structural and HVAC components in the building's mechanical floor.",
+     link:'/port_lurie_big.jpg',//set to no to hide
+      link_class:'icon-pic',
+      link_txt:'View full-size',
+    },
+    {
+      title: 'Intelligent Buildings',
+      subtitle: "Booklet Design & Illustration",
+      image:'port_ib2.jpg',
+      desc:"This booklet was created as marketing collateral for Affiliated Engineers Inc.'s Intelligent Building (IB) Group. The IB group specializes in optimizing data to improve building energy effiency and lower maintenance costs. In addition to designing the layout of the booklet, I also drew all of the icons, illustrations, and infographics.",
+     link:'http://issuu.com/kyla.v/docs/ib_booklet?workerAddress=ec2-54-224-253-214.compute-1.amazonaws.com',//set to no to hide
+      link_class:'icon-link',
+      link_txt:'Read the booklet',
+    },
+    {
+      title: 'Doomhold',
+      subtitle: "Video game development",
+      image:'port_vgame.jpg',
+      desc:"Doomhold is a Retro action/RPG video game curently in development. I am doing all the programming as well as creating all of the art assets for the project.  The demo is currently avaliable for window 7+.",
+     link:'http://kylavana.github.io/doomhold_demo_1.0.0.3.zip',//set to no to hide
+      link_class:'icon-dl',
+      link_txt:'Download the demo',
+    },
+    {
+      title: 'Anger',
+      subtitle: 'Linocut',
+      image:'port_fist.jpg',
+      desc:'Personal project - linocut is similar to woodcut in that the design is cut into the linoleum with a chisel. The uncarved areas represent a mirror image of the parts that are to be printed. The surface is then coated with ink, and pressed into paper or fabric. The reductive process was a fun challenge!',
+     link:'no',//set to no to hide
+      link_class:'',
+      link_txt:'',
+    },
+    {
+      title: 'Cardiff Castle',
+      subtitle: "Charcol Sketch",
+      image:'port_castle.jpg',
+      desc:"An illustration done for Affiliated Engineers Inc. of the mechanical level in the Ann & Robert H. Lurie Children's Hospital of Chicago. The hospital is noteworthy for it's vertical orientation (hospitals are almost always laid-out horizontally) and for the complex interaction between structural and HVAC components in the building's mechanical floor.",
+     link:'no',//set to no to hide
+      link_class:'',
+      link_txt:'',
+    },
+    {
+      title: 'Waste to Energy',
+      subtitle: "Illustration",
+      image:'port_comic.jpg',
+      desc:"This illustration was drawn for Affiliated Engineers, Inc. and shows how waste products are used in the creation of a renewable energy source. I used a comic book style to make the complex process more accessible to a non-technical audience.",
+     link:'no',//set to no to hide
+      link_class:'',
+      link_txt:'',
+    },
+  ];
+});
+// End Angular Module
